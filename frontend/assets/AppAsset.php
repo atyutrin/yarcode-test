af087b280b41a0c419a77ce078cccd865f1d3669
Aleksey Tyutrin <atutrin@gmail.com>
2017-11-18 19:33:19 +0800

    Подключил скрипты и стили

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,9 +12,16 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/agency.css',
+        'https://fonts.googleapis.com/css?family=Montserrat:400,700',
+        'https://fonts.googleapis.com/css?family=Kaushan+Script',
+        'https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic',
+        'https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700',
     ];
     public $js = [
+        'js/agency.js',
+        'js/contact_me.js',
+        'js/jqBootstrapValidation.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
