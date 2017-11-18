f87f8269e289c0e7b655e37dbc63a22502ccbac8
Aleksey Tyutrin <atutrin@gmail.com>
2017-11-18 19:34:34 +0800

    Изменил виджет меню

--- common/widgets/NavBarAgency.php
+++ common/widgets/NavBarAgency.php
@@ -8,8 +8,20 @@
 
 namespace common\widgets;
 
+use yii\bootstrap\NavBar;
+use yii\bootstrap\Html;
 
-class NavBarAgency
+class NavBarAgency extends NavBar
 {
+    protected function renderToggleButton()
+    {
+        $bar = Html::tag('i', '', ['class' => 'fa fa-bars']);
+        $screenReader = "<span class=\"sr-only\">{$this->screenReaderToggleText}</span> Menu ";
 
+        return Html::button("{$screenReader}\n{$bar}", [
+            'class' => 'navbar-toggle',
+            'data-toggle' => 'collapse',
+            'data-target' => "#{$this->containerOptions['id']}",
+        ]);
+    }
 }
